ed50076c64077f0505d586734c62b64bf3008eb6
Swathi Pejakala <swathi.rao.pejakala@gmail.com>
2018-11-25 22:46:08 -0600

    Updates

---
+++ precache-manifest.4b2247e38ad652923bebee0f1a0fa370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a4430c086131fba29bb",
+    "url": "/TicTacToe/static/js/runtime~main.1a4430c0.js"
+  },
+  {
+    "revision": "0f5039378cb8b1b3cea7",
+    "url": "/TicTacToe/static/js/main.0f503937.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/TicTacToe/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "0f5039378cb8b1b3cea7",
+    "url": "/TicTacToe/static/css/main.84ba813d.chunk.css"
+  },
+  {
+    "revision": "55b2e195634fa081023d94fb890df430",
+    "url": "/TicTacToe/index.html"
+  }
+];
